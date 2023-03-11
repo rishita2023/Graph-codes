c97c7109568d88e28bc5482152c0207f45fc37c1
Rishita Bose <92913222+rishita2023@users.noreply.github.com>
2023-03-11 20:36:47 +0530

    Update TopologicalSort.cpp

--- TopologicalSort.cpp
+++ TopologicalSort.cpp
@@ -1,4 +1,5 @@
 /*Linear ordering list- if there is an edge btw u & v, u appears before v in that ordering*/
+/* DFS */
 void dfs(int node, int vis[],stack<int>s, vector<int>adj[]){
   vis[node]=1;
   for(auto it: adj[node]){
@@ -27,3 +28,36 @@ vector<int>toposort(int V, vector<int>adj[])
 }
     
   /* real life examples - Operation System deadlock detection, data serialization */
+
+/*Kahns Algorithm bfs method (Indegree calculation) */
+vector<int>toposort(int V, vector<int>adj[]){
+int indegree[V] ={0};
+for(int i=0;i<V;i++){
+  for(auto it:adj[i]){
+    indegree[it]++;
+  }
+}
+queue<int>q;
+for(int i=0;i<V;i++){
+  if(indegree[i]==0){
+    q.push(i);
+  }
+}
+vector<int>tops;
+while(!q.empty()){
+  int node=q.front();
+  q.pop();
+  tops.push_back(node);
+  for(auto it: adj[node]){
+    indegree[it]--;
+    if(indegree[it]==0){
+      q.push(it);
+    }
+  }
+}
+return tops;
+}
+
+
+
+
